937efbb3432b1de134f5eac01ddce1a37e8f5d43
Domischwarz-projects <accounting@ku-ro.de>
2020-12-14 16:47:48 +0100

    secure google api key
    secure google api key

--- frontend/src/pages/Maps.js
+++ frontend/src/pages/Maps.js
@@ -1,66 +1,33 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '../../node_modules/@react-google-maps/api';
-import { Circle } from '../../node_modules/@react-google-maps/api';
+import { GoogleMap, useLoadScript } from '../../node_modules/@react-google-maps/api';
+
+//import { formatRelative } from 'date-fns'
 
 
-//Circle Marker Restaurant
+const  libraries = ['places'];
+
 const center = {
     lat: 48.12895412211271,
     lng: 11.568265640300746
   }
+const mapStyles = {width: '100vw',height: '100vh'};
 
-  const options = {
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-    radius: 50,
-    zIndex: 1
-  }
-  
-  const onLoad = circle => {
-    console.log('Circle onLoad circle: ', circle)
-  }
-  
-  const onUnmount = circle => {
-    console.log('Circle onUnmount circle: ', circle)
-  }
+export default function Map() {
 
+	const { isLoaded, loadError } = useLoadScript({
+		googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+		libraries
+	});
+	if (loadError) return "Error loading maps";
+	if (!isLoaded) return "Loading Maps";
 
+	return (
 
-export default function aufDieFaustMap() {
-    return (
-      <LoadScript googleMapsApiKey="AIzaSyCbr81eikRxUAqmOypGYTQhA_d75Mlpj7w">
-        <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={15}>
-            { /* Child components, such as markers, info windows, etc. */ }
-            <Circle
-      // optional
-      onLoad={onLoad}
-      // optional
-      onUnmount={onUnmount}
-      // required
-      center={center}
-      // required
-      options={options}
-    />
-        </GoogleMap>
-      </LoadScript>
-    )
-  }
-  
- 
-  
-
-//////////////////////////////////////////////
-//Styling
-
-//Google Maps
-const mapStyles = {
-  width: '100vw',
-  height: '100vh'
+		<GoogleMap
+			mapContainerStyle={mapStyles}
+			zoom={10}
+			center={center}
+			
+		></GoogleMap>
+	)
 }
